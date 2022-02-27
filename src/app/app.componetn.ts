a40a9b58f84bdeb776e86702af5a85bee1e541d3
Davide Fassola <davide.fassola@deltatre.com>
2022-02-27 16:56:09 +0100

    added bootstrapping and app.component.ts to show an H1

---
+++ src/app/app.componetn.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-root',
+    template:'<h1>My App</h1>',
+    styles: ['h1{color:white;}']
+})
+
+export class AppComponent {}
